16630134d525870f1d375ffa86a7ea7bb91d1d22
= <=>
2020-11-24 15:54:48 +0100

    changed sender email to no.reply.service.finder@gmail.com

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'foulene999@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no.reply.service.finder@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Service Finder'),
     ],
 
