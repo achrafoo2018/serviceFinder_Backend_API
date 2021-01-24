a48e1805985346aab18876c2240398b5cabcd3ce
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2021-01-24 22:51:21 +0100

    admin fixed

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,6 +11,6 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
-    $router->resource('/users', UserController::class);
+    $router->resource('/Users', UserController::class);
 
 });
